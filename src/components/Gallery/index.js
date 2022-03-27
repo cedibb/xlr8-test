86db81a8a58f4d5067a0a2e93ba62089902e2327
Carlos Dibb <carlos@dibb.dev>
2022-03-27 13:14:21 -0300

    Container component test

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,11 +1,12 @@
 import Image from "../Image";
+import Container from "../Container";
 
 const Gallery = ({ children, bookmarkImage }) => {
   const imageComponents = children.map((data, i) => (
     <Image data={data} bookmark={bookmarkImage} key={i} />
   ));
 
-  return <div className="container">{imageComponents}</div>;
+  return <Container>{imageComponents}</Container>;
 };
 
 export default Gallery;
